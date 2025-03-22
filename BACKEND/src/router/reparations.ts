d700de5924240fa68e8b4a90aac5eabf30d56d68
Sabrina <sabrinah_38@hotmail.fr>
2025-03-22 14:39:13 +0100

    mise en place prisma / connection dbeaver

--- BACKEND/src/router/reparations.ts
+++ BACKEND/src/router/reparations.ts
@@ -1,43 +1,43 @@
-import { Router } from "express";
-import { PrismaClient } from "@prisma/client";
+// import { Router } from "express";
+// import { PrismaClient } from "@prisma/client";
 
-export const reparationRouter = Router();
-const prisma = new PrismaClient();
+// export const reparationRouter = Router();
+// const prisma = new PrismaClient();
 
-// POST
-reparationRouter.post('/', async (req, res) => {
-  const reparation = await prisma.reparation.create({
-    data : {
-      name:req.body.data.name,
-      prix: req.body.data.prix,
-      instrumentId: req.body.data.instrumentId
-    }
-  });
-  res.status(201).json(reparation);
-})
+// // POST
+// reparationRouter.post('/', async (req, res) => {
+//   const reparation = await prisma.reparation.create({
+//     data : {
+//       name:req.body.data.name,
+//       prix: req.body.data.prix,
+//       instrumentId: req.body.data.instrumentId
+//     }
+//   });
+//   res.status(201).json(reparation);
+// })
 
 
-//DELETE
-reparationRouter.delete("/:id", async (req, res) => {
-    const reparationId = parseInt(req.params.id);
+// //DELETE
+// reparationRouter.delete("/:id", async (req, res) => {
+//     const reparationId = parseInt(req.params.id);
   
-    if (isNaN(reparationId)) {
-      return res.status(400).json({ message: "Invalid Reparation ID" });
-    }
+//     if (isNaN(reparationId)) {
+//       return res.status(400).json({ message: "Invalid Reparation ID" });
+//     }
   
-    const reparation = await prisma.reparation.findUnique({
-      where: { id: reparationId },
-    });
+//     const reparation = await prisma.reparation.findUnique({
+//       where: { id: reparationId },
+//     });
   
-    if (!reparation) {
-      return res.status(404).json({ message: "Reparation not found" });
-    }
+//     if (!reparation) {
+//       return res.status(404).json({ message: "Reparation not found" });
+//     }
   
-    await prisma.reparation.delete({
-      where: { id: reparationId },
-    });
+//     await prisma.reparation.delete({
+//       where: { id: reparationId },
+//     });
   
-    res.json({ message: "Reparation deleted" });
-  });
+//     res.json({ message: "Reparation deleted" });
+//   });
   
 
--- BACKEND/src/router/users.ts
+++ BACKEND/src/router/users.ts
@@ -1,113 +1,113 @@
-import { Router } from "express";
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+// import { Router } from "express";
+// import { PrismaClient } from "@prisma/client";
+// import bcrypt from "bcrypt";
+// import jwt from "jsonwebtoken";
 
-export const userRouter = Router();
-const prisma = new PrismaClient();
-
-
-// POST
-userRouter.post('/register', async (req, res) => {
-  const { pseudo } = req.body.data;  
-  const userWithPseudo = await prisma.user.findFirst({ where: { pseudo } });
-  if (userWithPseudo) {
-      return res.status(400).json("Pseudo already use");  
-  } else {
-      const hashedPassword = await bcrypt.hash(req.body.data.motdepasse, 10);
-      const newUser = await prisma.user.create({
-          data: {
-              pseudo: req.body.data.pseudo,
-              motdepasse: hashedPassword
-          }
-      });
-
-      const token = jwt.sign({ pseudo }, process.env.JWT_SECRET!);
-      return res.status(201).json({token});
-  }
-});
-
-// GET
-userRouter.get("/", async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-})
+// export const userRouter = Router();
+// const prisma = new PrismaClient();
 
 
-//GET ID
-userRouter.get("/:id", async (req, res) => {
-  const userId = parseInt(req.params.id);
+// // POST
+// userRouter.post('/register', async (req, res) => {
+//   const { pseudo } = req.body.data;  
+//   const userWithPseudo = await prisma.user.findFirst({ where: { pseudo } });
+//   if (userWithPseudo) {
+//       return res.status(400).json("Pseudo already use");  
+//   } else {
+//       const hashedPassword = await bcrypt.hash(req.body.data.motdepasse, 10);
+//       const newUser = await prisma.user.create({
+//           data: {
+//               pseudo: req.body.data.pseudo,
+//               motdepasse: hashedPassword
+//           }
+//       });
 
-  if (isNaN(userId)) {
-    return res.status(400).json({ message: "Invalid user ID" });
-  }
+//       const token = jwt.sign({ pseudo }, process.env.JWT_SECRET!);
+//       return res.status(201).json({token});
+//   }
+// });
 
-  const user = await prisma.user.findUnique({
-    where: { id: userId },
-  });
-
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
-
-  res.json(user);
-});
-
-
-//DELETE
-userRouter.delete("/:id", async (req, res) => {
-  const userId = parseInt(req.params.id);
-
-  if (isNaN(userId)) {
-    return res.status(400).json({ message: "Invalid user ID" });
-  }
-
-  const user = await prisma.user.findUnique({
-    where: { id: userId },
-  });
-
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
-
-  await prisma.user.delete({
-    where: { id: userId },
-  });
-
-  res.json({ message: "User deleted" });
-});
+// // GET
+// userRouter.get("/", async (req, res) => {
+//   const users = await prisma.user.findMany();
+//   res.json(users);
+// })
 
 
-//PUT 
-userRouter.put("/:id", async (req, res) => {
-    const userId = parseInt(req.params.id);
+// //GET ID
+// userRouter.get("/:id", async (req, res) => {
+//   const userId = parseInt(req.params.id);
+
+//   if (isNaN(userId)) {
+//     return res.status(400).json({ message: "Invalid user ID" });
+//   }
+
+//   const user = await prisma.user.findUnique({
+//     where: { id: userId },
+//   });
+
+//   if (!user) {
+//     return res.status(404).json({ message: "User not found" });
+//   }
+
+//   res.json(user);
+// });
+
+
+// //DELETE
+// userRouter.delete("/:id", async (req, res) => {
+//   const userId = parseInt(req.params.id);
+
+//   if (isNaN(userId)) {
+//     return res.status(400).json({ message: "Invalid user ID" });
+//   }
+
+//   const user = await prisma.user.findUnique({
+//     where: { id: userId },
+//   });
+
+//   if (!user) {
+//     return res.status(404).json({ message: "User not found" });
+//   }
+
+//   await prisma.user.delete({
+//     where: { id: userId },
+//   });
+
+//   res.json({ message: "User deleted" });
+// });
+
+
+// //PUT 
+// userRouter.put("/:id", async (req, res) => {
+//     const userId = parseInt(req.params.id);
   
-    if (isNaN(userId)) {
-      return res.status(400).json({ message: "Invalid user ID" });
-    }
+//     if (isNaN(userId)) {
+//       return res.status(400).json({ message: "Invalid user ID" });
+//     }
   
-    const user = await prisma.user.findUnique({
-      where: { id: userId },
-    });
+//     const user = await prisma.user.findUnique({
+//       where: { id: userId },
+//     });
   
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+//     if (!user) {
+//       return res.status(404).json({ message: "User not found" });
+//     }
   
-    let hashedPassword = user.pseudo; 
+//     let hashedPassword = user.pseudo; 
   
-    if (req.body.data.mtp) {
-      hashedPassword = await bcrypt.hash(req.body.data.pseudo, 10);
-    }
+//     if (req.body.data.mtp) {
+//       hashedPassword = await bcrypt.hash(req.body.data.pseudo, 10);
+//     }
   
-    const updatedUser = await prisma.user.update({
-      where: { id: userId },
-      data: {
-        pseudo: req.body.data.pseudo,
-        motdepasse: hashedPassword,
-      },
-    });
+//     const updatedUser = await prisma.user.update({
+//       where: { id: userId },
+//       data: {
+//         pseudo: req.body.data.pseudo,
+//         motdepasse: hashedPassword,
+//       },
+//     });
   
-    res.json({ message: "User updated" });
-  });
+//     res.json({ message: "User updated" });
+//   });
   
