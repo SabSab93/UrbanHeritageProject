9be643f7f292487b1cb3b04c87a76a01c80f7c2c
Sabrina <sabrinah_38@hotmail.fr>
2025-04-10 21:23:38 +0200

    update gitignore add .env

--- BACKEND/start.sh
+++ BACKEND/start.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-# Attendre que la base de données soit prête
-echo "⏳ Attente de la base de données..."
-sleep 5
-
-# Générer le client Prisma dans le container
-echo "🔧 Génération du client Prisma..."
+echo "📦 Prisma generate"
 npx prisma generate
 
-# Exécuter les migrations (optionnel si tu veux)
-# npx prisma migrate deploy
+echo "⚙️ Prisma migrate deploy"
+npx prisma migrate deploy
 
-# Lancer le serveur Node (à adapter selon ton projet)
-echo "🚀 Lancement de l'application..."
-npm run start
+echo "🚀 Starting backend"
+npm run dev
