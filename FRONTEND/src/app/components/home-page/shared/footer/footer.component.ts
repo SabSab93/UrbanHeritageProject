3d6b13cf0754109a425d75b490234c9c955f79d9
Sabrina <sabrinah_38@hotmail.fr>
2025-05-01 16:07:33 +0200

    mise à jour scss

--- FRONTEND/src/app/components/home-page/shared/footer/footer.component.ts
+++ FRONTEND/src/app/components/home-page/shared/footer/footer.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
   selector: 'app-footer',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './footer.component.html'
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent { }
