717917c894a5e02dd49324f6d807b74fbec930ae
Sabrina <sabrinah_38@hotmail.fr>
2025-05-01 11:45:31 +0200

    creation header

---
+++ FRONTEND/src/app/components/home-page/shared/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+  activeLink = 'accueil';
+
+  setActive(link: string) {
+    this.activeLink = link;
+  }
+}
