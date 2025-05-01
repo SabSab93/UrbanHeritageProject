94cd7201e3121e84eabf4e07d098d98cd2031dd0
Sabrina <sabrinah_38@hotmail.fr>
2025-05-01 11:45:40 +0200

    ajout header dans home page

--- FRONTEND/src/app/components/home-page/home-page.component.ts
+++ FRONTEND/src/app/components/home-page/home-page.component.ts
@@ -4,11 +4,12 @@ import { RouterModule } from '@angular/router';
 import { SectionEngagementsComponent } from './sections/section-engagements/section-engagements.component';
 import { SectionIntroductionComponent } from './sections/section-introduction/section-introduction.component';
 import { SectionAProposComponent } from './sections/section-a-propos/section-a-propos.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, RouterModule, SectionEngagementsComponent,SectionIntroductionComponent,SectionAProposComponent],
+  imports: [CommonModule, RouterModule, HeaderComponent, SectionEngagementsComponent,SectionIntroductionComponent,SectionAProposComponent],
   templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {}
