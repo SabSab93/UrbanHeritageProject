abece53f3dcd155692ca251b07a739deba52036f
Sabrina <sabrinah_38@hotmail.fr>
2025-05-08 11:54:31 +0200

    refacto app header

--- FRONTEND/src/app/components/home-page/sections/section-introduction/section-introduction.component.ts
+++ FRONTEND/src/app/components/home-page/sections/section-introduction/section-introduction.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-section-introduction',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './section-introduction.component.html'
 })
 export class SectionIntroductionComponent {}
