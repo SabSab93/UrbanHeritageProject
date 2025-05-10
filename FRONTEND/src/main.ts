6a27dbdcfb321c95e7a4603d3055865fd307847b
Sabrina <sabrinah_38@hotmail.fr>
2025-05-10 08:28:29 +0200

    ajout local fr

--- FRONTEND/src/main.ts
+++ FRONTEND/src/main.ts
@@ -1,3 +1,4 @@
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent }         from './app/app.component';
 import { provideHttpClient, withInterceptors }    from '@angular/common/http';
@@ -5,13 +6,21 @@ import { provideRouter }        from '@angular/router';
 import { routes }               from './app/app.routes';
 import { authTokenInterceptor } from './core/interceptors/auth-token.interceptor';
 
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { LOCALE_ID } from '@angular/core';
+
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+registerLocaleData(localeFr);
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(
       withInterceptors([ authTokenInterceptor ])
     ),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'fr' }
   ]
-}).catch(err => console.error(err));
+})
+.catch(err => console.error(err));
