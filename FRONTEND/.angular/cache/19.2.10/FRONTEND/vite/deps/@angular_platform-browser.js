3b7312eafa73d60c8abbc2621a436a3b64afe8e3
Sabrina <sabrinah_38@hotmail.fr>
2025-05-16 16:04:33 +0200

    add Dockerfile backend

--- FRONTEND/.angular/cache/19.2.10/FRONTEND/vite/deps/@angular_platform-browser.js
+++ FRONTEND/.angular/cache/19.2.10/FRONTEND/vite/deps/@angular_platform-browser.js
@@ -41,8 +41,8 @@ import {
 } from "./chunk-J4PXBOAM.js";
 import "./chunk-6LE3XCSV.js";
 import "./chunk-NYFJF33E.js";
-import "./chunk-PEBH6BBU.js";
 import "./chunk-WPM5VTLQ.js";
+import "./chunk-PEBH6BBU.js";
 import "./chunk-4S3KYZTJ.js";
 import "./chunk-3OV72XIM.js";
 export {
