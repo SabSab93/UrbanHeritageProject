9a4c20f73d72cfd1c7c38ea2174a69704116b4c4
Sabrina <sabrinah_38@hotmail.fr>
2025-05-17 14:25:11 +0200

    update auth-token

--- FRONTEND/src/core/interceptors/auth-token.interceptor.ts
+++ FRONTEND/src/core/interceptors/auth-token.interceptor.ts
@@ -1,16 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Injectable } from '@angular/core';
 
-@Injectable({providedIn: 'root'})
-export class ClientService {
-  constructor(private http: HttpClient) {}
-
-  register(data: any) {
-    // *PAS* `/client/register-client` si ton back attend POST /api/client/register
-    return this.http.post(
-      `${environment.apiUrl}/client/register`,
-       data
-    );
+export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const apiUrl = environment.apiUrl;
+  if (req.url.startsWith(apiUrl)) {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      req = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
+    }
   }
-}
+  return next(req);
+};
