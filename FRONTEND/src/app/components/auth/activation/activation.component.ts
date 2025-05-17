1d571d76f1e56dcf6953ef5358cef60ca14988ed
Sabrina <sabrinah_38@hotmail.fr>
2025-05-17 16:18:05 +0200

    update redirect template

--- FRONTEND/src/app/components/auth/activation/activation.component.ts
+++ FRONTEND/src/app/components/auth/activation/activation.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { AuthLoginService } from '../../../services/auth-service/auth-login.service'; 
 import { Client } from '../../../models/client.model';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-activation',
@@ -16,24 +17,27 @@ export class ActivationComponent implements OnInit {
     private authLoginService: AuthLoginService // pour login auto
   ) {}
 
-  ngOnInit(): void {
+   ngOnInit(): void {
     const token = this.route.snapshot.queryParamMap.get('token');
-    if (token) {
-        this.http.post<{ message: string, token: string, client: Client }>(
-            `http://localhost:1992/api/auth/activate/${token}`, {}
-          ).subscribe({
-            next: (res) => {
-              this.authLoginService.loginManuel(res.token, res.client); 
-              this.router.navigate(['/'], { queryParams: { activated: '1' } });
-            },
-            error: (err) => {
-              this.router.navigate(['/'], {
-                queryParams: { activated: '0', error: err?.error?.message }
-              });
+    if (!token) {
+      return ;
+    }
+
+    const url = `${environment.apiUrl}/auth/activate/${token}`;
+    this.http.post<{ message: string; token: string; client: Client }>(url, {})
+      .subscribe({
+        next: ({ token, client }) => {
+          this.authLoginService.loginManuel(token, client);
+          this.router.navigate(['/'], { queryParams: { activated: '1' } });
+        },
+        error: (err) => {
+          this.router.navigate(['/'], {
+            queryParams: {
+              activated: '0',
+              error: err?.error?.message ?? 'Erreur inconnue'
             }
           });
-    } else {
-      this.router.navigate(['/'], { queryParams: { activated: '0' } });
-    }
+        }
+      });
   }
 }
