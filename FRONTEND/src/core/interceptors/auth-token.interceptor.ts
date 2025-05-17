804588ac0d667679d5c9519a733ca35c56c25ad9
Sabrina <sabrinah_38@hotmail.fr>
2025-05-17 13:18:33 +0200

    change angular.json

--- FRONTEND/src/core/interceptors/auth-token.interceptor.ts
+++ FRONTEND/src/core/interceptors/auth-token.interceptor.ts
@@ -1,17 +1,12 @@
-import { HttpRequest, HttpHandlerFn, HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
-  // On ne touche qu’aux requêtes vers notre API
-  if (req.url.startsWith(environment.apiUrl)) {
+  const apiUrl = environment.apiUrl;
+  if (req.url.startsWith(apiUrl)) {
     const token = localStorage.getItem('authToken');
-    console.log('[INT] appel vers API', req.url, 'token=', !!token);
     if (token) {
-      req = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
-      });
+      req = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
     }
   }
   return next(req);
