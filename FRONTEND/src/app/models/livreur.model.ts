0f0c6d364bca3f7167cdbfd41962cabc3241ba34
Sabrina <sabrinah_38@hotmail.fr>
2025-05-20 13:37:21 +0200

    ajout model

---
+++ FRONTEND/src/app/models/livreur.model.ts
@@ -0,0 +1,6 @@
+
+export interface Livreur {
+  id_livreur: number;
+  nom_livreur: string;
+  Livraison?: any[];
+}
---
+++ FRONTEND/src/app/models/methode-livraison.model.ts
@@ -0,0 +1,5 @@
+export interface MethodeLivraison {
+  id_methode_livraison: number;
+  nom_methode: string;
+  prix_methode: number;
+}
